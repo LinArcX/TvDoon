ef00ca54e1229e0db1a185c8269ec7ec76545570
LinArcX <linarcx@gmail.com>
2017-03-24 12:50:08 +0430

    more clean up!

--- setup.py
+++ setup.py
@@ -1,18 +1,6 @@
 import os
 from setuptools import setup
 
-# Utility function to read the README file.
-# Used for the long_description.  It's nice, because now 1) we have a top level
-# README file and 2) it's easier to type in the README file than to put a raw
-# string in below ...
-#def read(fname):
- #   return open(os.path.join(os.path.dirname(__file__), fname)).read()
-
-# requirements = [         
-#                     'tvdb_api',
-#                     'PTable',
-#                 ]
-
 setup(
     name = "tvdoon",
     version = "0.1",
@@ -22,11 +10,9 @@ setup(
     license = "GPL3",
     keywords = "tvShow, upcoming tvshow",
     url = "http://packages.python.org/an_example_pypi_project",
-    packages=['tvdoon'],
-    #install_requires=requirements,
-    #install_requires= ['tvdb_api','PTable','prettytable'],
-    install_requires= ['tvdb_api','prettytable'],
-    setup_requires= ['tvdb_api', 'prettytable'],
+    packages=['tvdoon','util'],
+    install_requires= ['tvdb_api','prettytable', 'pyfiglet '],
+    setup_requires= ['tvdb_api', 'prettytable', 'pyfiglet '],
     scripts=['tvdoon/tvdoon'],
     classifiers=[
         "Development Status :: 3 - Alpha",
