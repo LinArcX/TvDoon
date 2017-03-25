c620d809f5a5c1b508377f8879fbc60742c76728
LinArcX <linarcx@gmail.com>
2017-03-25 20:18:49 +0430

    fix bug pid=6491#p6491

--- setup.py
+++ setup.py
@@ -1,18 +1,17 @@
-import os
 from setuptools import setup
 
 setup(
-    name = "tvdoon",
-    version = "0.1",
-    author = "LinArcX",
-    author_email = "linarcx@gmail.com",
-    description = ("Show Information About Tv Show's, Ratings And So Many Cool Features!"),
-    license = "GPL3",
-    keywords = "tvShow, upcoming tvShow",
-    url = "http://packages.python.org/an_example_pypi_project",
+    name="tvdoon",
+    version="0.1",
+    author="LinArcX",
+    author_email="linarcx@gmail.com",
+    description="Show Information About Tv Show's, Ratings And So Many Cool Features!",
+    license="GPL3",
+    keywords="tvShow, upcoming tvShow",
+    url="http://packages.python.org/an_example_pypi_project",
     packages=['tvdoon'],
     # install_requires= ['tvdb_api','prettytable', 'pyfiglet'],
-    # setup_requires= ['tvdb_api', 'prettytable', 'pyfiglet'],
+    # setup_requires = ['tvdb_api', 'prettytable', 'pyfiglet'],
     scripts=['tvdoon/tvdoon'],
     classifiers=[
         "Development Status :: 3 - Alpha",
