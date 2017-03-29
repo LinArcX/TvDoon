6d36bfd8a984731c59e1fe1d38582d6de544e7d9
LinArcX <linarcx@gmail.com>
2017-03-29 11:33:27 +0430

    add util package and change dependency to pytvmaze

--- setup.py
+++ setup.py
@@ -2,16 +2,17 @@ from setuptools import setup
 
 setup(
     name="tvdoon",
-    version="0.3",
+    version="0.1.0",
     author="LinArcX",
     author_email="linarcx@gmail.com",
     description="Show Information About Tv Show's, Ratings And So Many Cool Features!",
     license=" GPLv3",
     keywords="tvShow, upcoming tvShow",
     url="https://github.com/LinArcX/tvdoon",
+    download_url='https://github.com/LinArcX/tvdoon/archive/0.1.0.tar.gz',
     packages=['tvdoon'],
-    install_requires= ['tvdb_api','prettytable', 'pyfiglet'],
-    setup_requires = ['tvdb_api', 'prettytable', 'pyfiglet'],
+    install_requires= ['tvdb_api','prettytable', 'pyfiglet', 'requests-cache'],
+    setup_requires = ['tvdb_api', 'prettytable', 'pyfiglet', 'requests-cache'],
     scripts=['tvdoon/tvdoon'],
     classifiers=[
         "Development Status :: 2 - Pre-Alpha",
