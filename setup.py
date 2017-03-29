6f4a46f25b0070169cac723c0fad3ea9f39e901f
LinArcX <linarcx@gmail.com>
2017-03-29 12:11:15 +0430

    change download url = 1.1.0

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ setup(
     license=" GPLv3",
     keywords="tvShow, upcoming tvShow",
     url="https://github.com/LinArcX/tvdoon",
-    download_url='https://github.com/LinArcX/tvdoon/archive/0.1.0.tar.gz',
+    download_url='https://github.com/LinArcX/tvdoon/archive/1.1.0.zip',
     packages=['tvdoon','utility'],
     install_requires= ['pytvmaze','prettytable', 'pyfiglet'],
     setup_requires = ['pytvmaze', 'prettytable', 'pyfiglet'],
